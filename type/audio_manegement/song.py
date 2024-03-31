f1034f0c2c7d13b75d70bc825bb208adb6582b8e
Warspyt <tatianax2442@gmail.com>
2024-03-31 09:26:31 -0500

    queries

--- type/audio_manegement/song.py
+++ type/audio_manegement/song.py
@@ -1,4 +1,5 @@
 import strawberry
+import typing
 import requests
 from server import url, audioManegement_port
 
@@ -6,6 +7,7 @@ api_url = f'http://{url}:{audioManegement_port}/songs'
 
 @strawberry.type
 class Song:
+    id: int
     title: str
     publicationDate: str
     lyrics: str
@@ -16,6 +18,7 @@ class Song:
 # Queries
 @strawberry.type
 class Query:
+    # Get song by id
     @strawberry.field    
     def song(self, id: int) -> Song:
         
@@ -25,16 +28,44 @@ class Query:
         if response.status_code == 200:
             # Devolver los datos obtenidos en formato JSON
             data = response.json()
+            
             return Song(
-                        title=data.get('title'),
-                        publicationDate=data.get('publicationDate'),
-                        lyrics=data.get('lyrics'),
-                        version=data.get('version'),
-                        userid=data.get('userid'),
-                        albumid=data.get('albumid')
+                        id = data.get('id'),
+                        title = data.get('title'),
+                        publicationDate = data.get('publicationDate'),
+                        lyrics = data.get('lyrics'),
+                        version = data.get('version'),
+                        userid = data.get('userid'),
+                        albumid = data.get('albumid')
                     )
         else:
             raise Exception(f'Error al obtener la canción con ID {id} desde el microservicio Audio Manegement')
+        
+    # Get all songs
+    @strawberry.field    
+    def songs(self) -> typing.List[Song]:
+        
+        # Hacer request en soUNd_AudioManegement_MS
+        response = requests.get(api_url)
+        
+        if response.status_code == 200:
+            # Devolver los datos obtenidos en formato JSON
+            data = response.json()
+            
+            return [
+                Song(
+                    id = song_data.get('id'),
+                    title = song_data.get('title'),
+                    publicationDate = song_data.get('publicationDate'),
+                    lyrics = song_data.get('lyrics'),
+                    version = song_data.get('version'),
+                    userid = song_data.get('userid'),
+                    albumid = song_data.get('albumid')
+                )
+                for song_data in data
+            ]
+        else:
+            raise Exception(f'Error al obtener las canciones desde el microservicio Audio Manegement')
     
 # Mutations
 @strawberry.type
