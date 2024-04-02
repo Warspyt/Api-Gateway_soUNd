84050b0e7c62e30b5684493bd5e2e7cf95df46e4
Manuel Martinez <mmartinezve@unal.edu.co>
2024-04-02 02:18:23 -0500

    Consumir servicio de streaming

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from schemas.audio_manegement.index import app_graphql
+from streaming.streaming import app_streaming
 
 app = FastAPI()
-
 app.include_router(app_graphql)
+app.include_router(app_streaming)
