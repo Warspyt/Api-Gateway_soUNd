f0ad67a97a5bd63209b98a85846b81280ef80cfc
Warspyt <tatianax2442@gmail.com>
2024-04-02 21:32:31 -0500

    erased things

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 from fastapi import FastAPI
 from schemas.Index import app_graphql
+from streaming.streaming import app_streaming
+import threading
+from RabbitMQ.Audio_manegement.server import consume_messages
+
 
 
 app = FastAPI()
@@ -7,5 +11,5 @@ app.include_router(app_graphql)
 app.include_router(app_streaming)
 
 # # Iniciar el consumidor de RabbitMQ en un hilo separado
-# consumer_thread = threading.Thread(target=consume_messages)
-# consumer_thread.start()   
+consumer_thread = threading.Thread(target=consume_messages)
+consumer_thread.start()   
