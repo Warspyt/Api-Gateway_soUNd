72967359052a31c632aa02ac31ca40c8e52e7c33
Manuel Martinez <mmartinezve@unal.edu.co>
2024-04-23 02:23:33 -0500

    Rabbit docker compose

--- main.py
+++ main.py
@@ -2,12 +2,12 @@ from fastapi import FastAPI
 from schemas.index import app_graphql
 from streaming.streaming import app_streaming
 import threading
-#from RabbitMQ.Audio_manegement.server import consume_messages
+from RabbitMQ.Audio_manegement.server import consume_messages
 
 app = FastAPI()
 app.include_router(app_graphql)
 app.include_router(app_streaming)
 
 # Iniciar el consumidor de RabbitMQ en un hilo separado
-#consumer_thread = threading.Thread(target=consume_messages)
-#consumer_thread.start()   
+consumer_thread = threading.Thread(target=consume_messages)
+consumer_thread.start()   
