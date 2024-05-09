8046aa5bcc7512eea96fa4b598310909e2923280
warspyt <tatianax2442@gmail.com>
2024-05-08 21:10:34 -0500

    image upload

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 from fastapi import FastAPI
 from schemas.index import app_graphql
 from streaming.streaming import app_streaming
+from audio_manegement.songs import song_img
 import threading
 from RabbitMQ.Audio_manegement.server import consume_messages
 
 app = FastAPI()
 app.include_router(app_graphql)
 app.include_router(app_streaming)
+app.include_router(song_img)
 
 # Iniciar el consumidor de RabbitMQ en un hilo separado
 consumer_thread = threading.Thread(target=consume_messages)
